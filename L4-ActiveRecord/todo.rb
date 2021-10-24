e3c4f316296f049f8a4f52dbc620f4942dbd73b2
BPadmaja <b.padmaja@gmail.com>
2021-10-24 14:25:15 +0530

    Update todo.rb

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -1,25 +1,19 @@
-require "./connect_db.rb"
-require "./todo.rb"
-
-connect_db!
-Todo.show_list
-root@DESKTOP-NRR7F6C:~# cat todo.rb
 class Todo < ActiveRecord::Base
-  def due_today?
-    due_date == Date.today
+  def self.overdue
+    where("due_date < ?", Date.today)
   end
 
-  def overdue?
-    due_date < Date.today
+  def self.due_today
+    where("due_date = ?", Date.today)
   end
 
-  def due_later?
-    due_date > Date.today
+  def self.due_later
+    where("due_date > ?", Date.today)
   end
 
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    display_date = due_today? ? nil : due_date
+    display_date = due_date == Date.today ? nil : due_date
     "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
@@ -31,6 +25,7 @@ class Todo < ActiveRecord::Base
     todo = Todo.find(id)
     todo.completed = true
     todo.save
+    todo
   end
 
   def self.to_displayable_list
@@ -41,18 +36,15 @@ class Todo < ActiveRecord::Base
     puts "My Todo-list\n\n"
 
     puts "Overdue\n"
-    overdue = all.filter { |todo| todo.overdue? }
     overdue.map { |todo| puts todo.to_displayable_string }.join("\n")
     puts "\n\n"
 
     puts "Due Today\n"
-    overdue = all.filter { |todo| todo.due_today? }
-    overdue.map { |todo| puts todo.to_displayable_string }.join("\n")
+    due_today.map { |todo| puts todo.to_displayable_string }.join("\n")
     puts "\n\n"
 
     puts "Due Later\n"
-    overdue = all.filter { |todo| todo.due_later? }
-    overdue.map { |todo| puts todo.to_displayable_string }.join("\n")
+    due_later.map { |todo| puts todo.to_displayable_string }.join("\n")
     puts "\n\n"
   end
 end
